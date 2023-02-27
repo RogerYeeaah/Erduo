9362bc54d1d3cb93cdaff29d665566c3082f85ab
RogerYeeaah <vamps1994@gmail.com>
2023-02-27 16:31:57 +0800

    增加work過濾功能

--- script/script.js
+++ script/script.js
@@ -1,6 +1,25 @@
-//分類初始化
-console.log('1');
+$(document).ready(function () {
+	$(window).scroll(function() {
+		var ww = $(window).scrollTop();
 
-$('a[data-filter]').click(function(){
-	console.log('1')
-})
+		if (ww > 100) {
+			$('.header').addClass('fix');
+		} else {
+			$('.header').removeClass('fix');
+		}
+	});
+
+	$('li.option').on('click', function() {
+		var currentCate = this.dataset.filter;
+		$(this).addClass('active').siblings().removeClass('active')
+		$('.work-card').each(function() {
+			var thisA = [this.dataset.cate.split(' ')]
+			console.log(this.dataset.cate.split(' ').includes(currentCate))
+			if(this.dataset.cate.split(' ').includes(currentCate)) {
+				$(this).show()
+			} else {
+				$(this).hide()
+			}
+		})
+	})
+});
